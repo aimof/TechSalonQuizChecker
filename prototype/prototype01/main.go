c40d47ddf0c8ad22dbc96a44f75681ca89bf608f
aimof <reception@go-fmt.com>
2016-10-26 13:45:30 +0900

    read command.txt

--- prototype/prototype01/main.go
+++ prototype/prototype01/main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"log"
+	"os"
 	"os/exec"
 )
 
 func main() {
-	var rowCmd string
-	fmt.Scan(&rowCmd)
+	rowCmd := readCommand()
 	modelAnswer := defModelAnswer()
 	correctAnswer := checkAnswer10times(rowCmd, modelAnswer)
 	for i, correct := range correctAnswer {
 		fmt.Printf("%d 回目のテストは %t です。\n", i+1, correct)
 	}
+}
+
+func readCommand() string {
+	filename := "command.txt"
+	fp, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fp.Close()
+	scanner := bufio.NewScanner(fp)
+	var rowCmd string
+	for scanner.Scan() {
+		rowCmd = scanner.Text()
+	}
+	if err = scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return rowCmd
 }
 
 func checkAnswer10times(rowCmd string, modelAnswer []modelCase) []bool {
